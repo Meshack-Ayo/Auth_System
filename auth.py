7d55b4144d16209ba7c9e8a2da59d80a7c19f123
Meshack-Ayo <meshackayo7@gmail.com>
2026-06-16 12:19:33 +0100

    password check

--- auth.py
+++ auth.py
@@ -1,7 +1,7 @@
 database = dict()
 
 database["daniel"] = "password123"
-def login(u_name,p_word):
+def login(u_name, p_word):
     if u_name in database.keys():
         password = database.grt(u_name)
         if password == None or password  != p_word:
@@ -15,6 +15,10 @@ def login(u_name,p_word):
 def singnup(_name, p_word,p_word2):
     if p_word != p_word2:
         print(" The two password do not match")
+    elif u_name in database.keys():
+        print("This username is taken")    
+    else:
+        database[u_name] = p_word    
 
 
 
--- main.py
+++ main.py
@@ -3,5 +3,7 @@ import auth
 
 username = "daniel"
 password = "Password1234"
+password2 = "password123"
 
 auth.login(username, password)
+auth.singnup(username, password, password2)
